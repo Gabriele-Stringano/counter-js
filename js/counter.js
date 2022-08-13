392ba5ab7a7345dd13526060cbcbcc66dca6897f
Gabriele-Stringano <gabrielestringano@gmail.com>
2022-08-13 23:55:28 +0200

    Additional features added
    Style of the page is now attractive
    buttons have been updated

--- js/counter.js
+++ js/counter.js
@@ -1,23 +1,41 @@
 let counter=0;
 num.textContent = counter;
 
-increment.addEventListener("click", function(){operation('add')});
-
-decrement.addEventListener("click", function(){operation('subtract')});
-
-reset.addEventListener("click", function(){operation('reset')});
-
-function operation(operator) {
-    switch (operator) {
-        case 'add':
-            counter= counter+1;
-        break;
-        case 'subtract':
-            counter--;
-        break;
-        case 'reset':
-            counter = 0;
-        break;
-    }
+function countplus(){
+  counter= counter+1;
     num.textContent = counter;
+}
+
+function countMinus(){
+  counter= counter-1;
+    num.textContent = counter;
+}
+
+ function plus() {
+   counter= counter+1;
+     num.textContent = counter;
+   intervalID = setInterval (countplus, 500);
 };
+
+function minus() {
+  counter= counter-1;
+    num.textContent = counter;
+  intervalID =  setInterval (countMinus, 500);
+};
+
+function stop() {
+  clearInterval(intervalID);
+};
+
+function countReset() {
+  counter=0;
+  num.textContent = counter;
+};
+
+increment.addEventListener("mousedown", plus);
+increment.addEventListener("mouseup", stop);
+
+decrement.addEventListener("mousedown", minus);
+decrement.addEventListener("mouseup", stop);
+
+reset.addEventListener("click",countReset);
