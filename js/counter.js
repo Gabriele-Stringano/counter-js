350968155ff707f6859a01837ce452d91efcd5a3
Gabriele-Stringano <gabrielestringano@gmail.com>
2022-08-14 19:22:04 +0200

    Minor changes

--- js/counter.js
+++ js/counter.js
@@ -1,37 +1,38 @@
 let counter=0;
-num.textContent = counter;
+num.textContent = counter; //display the number on the screen
 
-function countplus(){
+function countplus(){ //simple function 
   counter= counter+1;
     num.textContent = counter;
 }
 
-function countMinus(){
+function countMinus(){ //simple function
   counter= counter-1;
     num.textContent = counter;
 }
 
- function plus() {
+ function plus() { //function called by the eventListener by pressing the button
    counter= counter+1;
      num.textContent = counter;
    intervalID = setInterval (countplus, 500);
 };
 
-function minus() {
+function minus() { //function called by the eventListener by pressing the button
   counter= counter-1;
     num.textContent = counter;
   intervalID =  setInterval (countMinus, 500);
 };
 
-function stop() {
+function stop() { //function called by the eventListener when the button is released
   clearInterval(intervalID);
 };
 
-function countReset() {
+function countReset() { //function called by the eventListener by clicking on the button
   counter=0;
   num.textContent = counter;
 };
 
+//list of events:
 increment.addEventListener("mousedown", plus);
 increment.addEventListener("mouseup", stop);
 
